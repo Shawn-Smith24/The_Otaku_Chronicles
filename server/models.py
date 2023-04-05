14d04858058dcb29985a14ef4efb7dfbf3326c50
shawn <shawnsmith0024@gmail.com>
2023-04-05 16:55:21 -0400

    validations complete

--- server/models.py
+++ server/models.py
@@ -41,6 +41,65 @@ class User(db.Model, SerializerMixin):
     def __repr__(self):
         return f'User : {self.user_name}, {self.email}'
     
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('UTF-8')
+        
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.set_password(password)
+        
+    @validates('first_name')
+    def validate_first_name(self, key, first_name):
+        if not first_name:
+         raise AssertionError('No first_name provided')
+     
+        if not re.match("[a-zA-Z]+", first_name):
+            
+          raise AssertionError('Provided first_name is not an first_name') 
+      
+        return first_name
+    
+    @validates('last_name')
+    def validate_last_name(self, key, last_name):
+        if not last_name:
+         raise AssertionError('No last_name provided')
+     
+        if not re.match("[a-zA-Z]+", last_name):
+            
+          raise AssertionError('Provided last_name is not an last_name') 
+      
+        return last_name    
+    
+    @validates('user_name')
+    def validate_user_name(self, key, user_name):
+        if not user_name:
+         raise AssertionError('No user_name provided')
+     
+        if not re.match("[a-zA-Z0-9]+", user_name):
+            
+          raise AssertionError('Provided user_name is not an user_name') 
+      
+        return user_name
+    
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        if not email:
+         raise AssertionError('No email provided')
+     
+        if not re.match("[^@]+@[^@]+\.[^@]+", email):
+            
+          raise AssertionError('Provided email is not an email address') 
+      
+        return email
+    
 class Post(db.Model, SerializerMixin):
     serialize_only = ('id', 'text', 'author_id', 'comments', 'likes')
     
@@ -59,6 +118,17 @@ class Post(db.Model, SerializerMixin):
     
     def __repr__(self):
         return f'Post : {self.text}'
+    
+    @validates('text')
+    def validate_text(self, key, text):
+        if not text:
+         raise AssertionError('No text provided')
+     
+        if not re.match("[a-zA-Z0-9]+", text):
+            
+          raise AssertionError('Provided text is not an text') 
+      
+        return text
     
     
 class Like(db.Model, SerializerMixin):
@@ -94,7 +164,16 @@ class Comment(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-   
+    @validates('text')
+    def validate_text(self, key, text):
+        if not text:
+         raise AssertionError('No text provided')
+     
+        if not re.match("[a-zA-Z0-9]+", text):
+            
+          raise AssertionError('Provided text is not an text') 
+      
+        return text
     
 
     def __repr__(self):
